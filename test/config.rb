3ce7a5d87f8713792b167180122c9bce066729fb
cavis <ryancavis@gmail.com>
2017-03-22 10:43:54 -0600

    Update config loading

--- test/config.rb
+++ test/config.rb
@@ -1,13 +1,27 @@
-require 'http'
+require 'excon'
+require 'ostruct'
 
-# dovetail connection
-if ENV['DOVETAIL_HOST'].nil?
-  puts 'you must set DOVETAIL_HOST'
-  exit 255
+CONFIG = OpenStruct.new
+optional_envs = %w(DOVETAIL_PROD)
+
+# load env config
+File.open("#{File.dirname(__FILE__)}/../env-example", 'r').each_line do |line|
+  unless line.strip.empty? || line[0] == '#'
+    name = line.split('=').first
+    CONFIG[name] = ENV[name] unless ENV[name].nil? || ENV[name].empty?
+    abort "you must set #{name}" unless CONFIG[name] || optional_envs.include?(name)
+
+    # add scheme and check connection to hosts
+    if CONFIG[name] && name.match(/_HOST$/)
+      if CONFIG[name][0..3] != 'http'
+        scheme = CONFIG[name].match(/.*\.prxu?\.(?:org|tech)$/) ? 'https' : 'http'
+        CONFIG[name] = "#{scheme}://#{CONFIG[name]}"
+      end
+      begin
+        Excon.head(CONFIG[name])
+      rescue
+        abort "unable to connect to #{CONFIG[name]}"
+      end
+    end
+  end
 end
-HTTP.head(ENV['DOVETAIL_HOST'])
-
-# hosts
-DOVETAIL_HOST = ENV['DOVETAIL_HOST']
-DOVETAIL_PROD = ENV['DOVETAIL_PROD']
-FEEDER_HOST   = ENV['FEEDER_HOST']
