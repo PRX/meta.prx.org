52fef9093582dbb9f5fd800d4d5d095c9a114817
gcampo88 <gigi.campo@gmail.com>
2017-03-24 10:10:14 -0400

    Explicitly set to_sign to nothing for first test

--- test/upload/signature_test.rb
+++ test/upload/signature_test.rb
@@ -1,19 +1,19 @@
 require 'test_helper'
 
  describe 'upload-signature' do
+   let(:connection) { connection = Excon.new(CONFIG.UPLOAD_HOST) }
+
    it 'returns 400 with nothing to sign' do
-     resp = Excon.get(CONFIG.UPLOAD_HOST)
+     resp = connection.get(query: {to_sign: ''})
      resp.status.must_equal 400
    end
 
    it 'return plain text media type' do
-     connection = Excon.new(CONFIG.UPLOAD_HOST)
      resp = connection.get(query: {to_sign: 'hello'})
      resp.headers['content-type'].must_equal 'text/plain'
    end
 
    it 'return to correct signature' do
-     connection = Excon.new(CONFIG.UPLOAD_HOST)
      resp = connection.get(query: {to_sign: 'test'})
      resp.body.to_s.must_equal "TyhhPs0RA37JFn+0oWNdm25HgBc="
    end
