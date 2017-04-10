a83d68b22a924ad7f62dd16d0fe86e816a947898
Chris Kalafarski <chris@farski.com>
2017-04-10 12:07:31 -0400

    Allow for trailing dot in hosts being tested

--- test/support/config.rb
+++ test/support/config.rb
@@ -14,7 +14,7 @@ File.open("#{File.dirname(__FILE__)}/../../env-example", 'r').each_line do |line
     # add scheme and check connection to hosts
     if CONFIG[name] && name.match(/_HOST$/)
       if CONFIG[name][0..3] != 'http'
-        scheme = CONFIG[name].match(/.*\.prxu?\.(?:org|tech)$/) ? 'https' : 'http'
+        scheme = CONFIG[name].match(/.*\.prxu?\.(?:org|tech)\.?$/) ? 'https' : 'http'
         CONFIG[name] = "#{scheme}://#{CONFIG[name]}"
       end
       begin
