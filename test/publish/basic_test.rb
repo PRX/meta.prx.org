fb6fe806a464c212b5bfb54772c6cef4ef767d16
Sandi Barr <sandi.barr@prx.org>
2019-08-16 15:04:19 -0500

    reinstate publish tests

--- test/publish/basic_test.rb
+++ test/publish/basic_test.rb
@@ -22,10 +22,15 @@ describe :publish, :js do
       delete_test_series_and_episodes!
     end
 
-    it 'creates new series' do
+    it 'creates new series and episode' do
       skip "set BLACKBOX=1 to fully test integration environment" unless blackbox_required?
       series_url = create_series!
       series_url.must_match(/\/series\/\d+/)
+      random_str = SecureRandom.hex(10)
+      episode_url = create_episode!(series_url, random_str)
+      episode_url.must_match(/\/story\/\d+/)
+      mp3 = publish_fetch_rss_media_url(series_url)
+      assert(mp3.match(/\.mp3$/), 'Found mp3 url')
     end
   end
 end
