c0f28a722971669d527385f8d9d1df6b87c97bad
Chris Kalafarski <chris@farski.com>
2020-06-18 16:56:13 -0400

    Improve Porter noop test

--- test/porter/noop_test.rb
+++ test/porter/noop_test.rb
@@ -1,48 +1,50 @@
 require 'test_helper'
+require 'aws-sdk-states'
 require 'json'
 
-client = Aws::States::Client.new(
-  region: region_name,
-  credentials: credentials,
-  # ...
-)
+step_functions = Aws::States::Client.new
 
 describe :porter, :execution do
   describe :noop do
-    req = client.start_execution({
-      state_machine_arn: CONFIG.PORTER_STATE_MACHINE_ARN,
-      input: {
-        Job: {
-          Id: 'porter-test-no-op',
-          Source: {
-            Mode: 'HTTP',
-            URL: 'some-url'
+    it 'returns the basic execution output' do
+      req = step_functions.start_execution({
+        state_machine_arn: CONFIG.PORTER_STATE_MACHINE_ARN,
+        input: {
+          Job: {
+            Id: 'porter-test-no-op',
+            Source: {
+              Mode: 'HTTP',
+              URL: 'http://example.com/'
+            }
           }
-        }
-      }.to_json
-    })
-
-    max_retries = 5
-
-    begin
-      desc = client.describe_execution({
-        execution_arn: req.execution_arn,
+        }.to_json
       })
 
-      raise RequestError if desc.status == 'RUNNING'
-    rescue RequestError => e
-      if retries <= max_retries
-        retries += 1
-        sleep 2 ** retries
-        retry
-      else
-        raise "Timeout: #{e.message}"
-      end
-    end
+      max_retries = 5
+      retries = 0
 
-    desc['JobResult']['Job']['Id'].must_equal 'porter-test-no-op'
-    desc['JobResult']['State'].must_equal 'DONE'
-    desc['JobResult']['FailedTasks'].must_equal []
-    desc['JobResult']['TaskResults'].must_equal []
+      begin
+        desc = step_functions.describe_execution({
+          execution_arn: req.execution_arn,
+        })
+
+        raise RuntimeError if desc.status == 'RUNNING'
+      rescue RuntimeError => e
+        if retries <= max_retries
+          retries += 1
+          sleep 2 ** retries
+          retry
+        else
+          raise "Timeout: #{e.message}"
+        end
+      end
+
+      output = JSON.parse(desc.output)
+
+      output['JobResult']['Job']['Id'].must_equal 'porter-test-no-op'
+      output['JobResult']['State'].must_equal 'DONE'
+      output['JobResult']['FailedTasks'].must_equal []
+      output['JobResult']['TaskResults'].must_equal []
+    end
   end
 end
