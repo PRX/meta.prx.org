a45c9515aaa9aac80b48097131b111054b15adb2
cavis <ryancavis@gmail.com>
2021-09-20 15:01:21 -0600

    Switch to testing the DT3 CDN

--- test/support/dovetail_dsl.rb
+++ test/support/dovetail_dsl.rb
@@ -25,33 +25,20 @@ module Dovetail
     end
 
     def get_redirect_locations(path)
-      stag_program = path.split('/').first
-      prod_program = stag_program.gsub('prod_', '')
-      prod_path = path.gsub('prod_', '')
-      has_prefix = stag_program != prod_program
-
-      unique_filename = "test.#{SecureRandom.uuid}.#{path.split('/').last}"
-
       redirect = get_unique("#{CONFIG.DOVETAIL_HOST}/#{path}?noImp")
       redirect.status.must_equal 302
-      redirect.headers['x-not-impressed'].must_equal 'yes'
-      redirect.headers['location'].must_include "/#{stag_program}/"
+      redirect.headers.keys.must_include 'x-depressions'
       location = URI(redirect.headers['location'])
-      digest = location.path.split('/')[2]
 
-      prod_redirect = get_unique("#{CONFIG.DOVETAIL_PROD_HOST}/#{prod_path}?noImp")
+      prod_redirect = get_unique("#{CONFIG.DOVETAIL_PROD_HOST}/#{path}?noImp")
       if prod_redirect.status != 302
-        puts "  Arrangement not found: #{CONFIG.DOVETAIL_PROD_HOST}/#{prod_path}?noImp"
+        puts "  Arrangement not found: #{CONFIG.DOVETAIL_PROD_HOST}/#{path}?noImp"
       end
       prod_redirect.status.must_equal 302
-      prod_redirect.headers['x-not-impressed'].must_equal 'yes'
-      prod_redirect.headers['location'].must_include "/#{prod_program}/"
+      prod_redirect.headers.keys.must_include 'x-depressions'
       prod_location = URI(prod_redirect.headers['location'])
 
-      [
-        "#{location.scheme}://#{location.host}/#{stag_program}/#{digest}/#{unique_filename}",
-        "#{prod_location.scheme}://#{prod_location.host}/#{prod_program}/#{digest}/#{unique_filename}",
-      ]
+      ["#{location}&force", "#{prod_location}&force"]
     end
   end
 end
