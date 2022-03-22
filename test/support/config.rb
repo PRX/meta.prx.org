71122348200846a4e02580eaae86bb25e791a1de
cavis <ryancavis@gmail.com>
2022-03-22 07:59:45 -0600

    Log all envs

--- test/support/config.rb
+++ test/support/config.rb
@@ -1,12 +1,15 @@
 require 'excon'
 require 'ostruct'
 
+puts ENV.inspect
+
 CONFIG = OpenStruct.new
 optional_envs = %w(DOVETAIL_PROD_HOST)
 
 # load env config
 File.open("#{File.dirname(__FILE__)}/../../env-example", 'r').each_line do |line|
   unless line.strip.empty? || line[0] == '#'
+
     name = line.split('=').first
     CONFIG[name] = ENV[name] unless ENV[name].nil? || ENV[name].empty?
     abort "you must set #{name}" unless CONFIG[name] || optional_envs.include?(name)
